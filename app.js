cd07447e195d552fe719127bce9f4862a02d18dd
bhuvaneshwaran-kumar <bhuvaneshwarankumar2000@gmail.com>
2020-09-20 19:29:28 +0530

    soved issue-1 :- empty task can't get added

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ function createLists(event){
     event.preventDefault();  const lists = document.createElement("div");
     lists.classList.add("lists");
     const li = document.createElement("li");
+    if(data.value != ""){
     li.innerText = data.value;
-    
     const checkIcon = document.createElement("i");
     let classesToAdd = ['fa', 'fa-check-circle-o', 'fa-2x','fa-check'];
     checkIcon.classList.add(...classesToAdd);
@@ -30,7 +30,7 @@ function createLists(event){
     lists.appendChild(checkIcon);
     lists.appendChild(trashIcon);
     bottom.appendChild(lists)
-
+    }
     data.value = "";
 }
 function checkortrash(event){
